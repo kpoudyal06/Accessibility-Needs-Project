f245f203c7a7d7fdb1fb932f8d8e4baec5ed6cf6
Elliot Gobbert <elliotg2@umbc.edu>
2026-04-29 13:50:09 -0400

    updated comments

--- backend/clearBackend.sh
+++ backend/clearBackend.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+# Just clears the backend files -- should be used for testing only when doing iterative tests, and you need to clear the backend in between runs
 rm -rf /umbc/class/cmsc447sp26/common/Accessibility-Needs-Project/backend/accessibility.db
 rm -rf /umbc/class/cmsc447sp26/common/Accessibility-Needs-Project/backend/fileUploadLocation/*
