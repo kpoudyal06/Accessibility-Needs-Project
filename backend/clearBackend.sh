0c0f5b8a542204d43395fd1caac92600ec6c7f38
Elliot Gobbert <elliotg2@umbc.edu>
2026-04-29 13:25:16 -0400

    Added a clean up script and removed an outdated file

---
+++ backend/clearBackend.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+rm -rf /umbc/class/cmsc447sp26/common/Accessibility-Needs-Project/backend/accessibility.db
+rm -rf /umbc/class/cmsc447sp26/common/Accessibility-Needs-Project/backend/fileUploadLocation/*
